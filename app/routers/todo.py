09c58cd1dcb1761d0dd51e5ed0ff665d5b9979b2
target <aminesalem2223@gmail.com>
2025-06-03 16:47:17 +0100

    complete the crud operation

--- app/routers/todo.py
+++ app/routers/todo.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from ..database import get_db
 from ..models import Todo
-from ..schemas import TodoCreate, User, Todo as TodoSchema
+from ..schemas import TodoCreate, User, Todo as TodoSchema, TodoUpdate
 from ..dependencies import get_current_active_user
 
 router = APIRouter(tags=["todos"])
@@ -18,3 +18,29 @@ def create_todo(todo: TodoCreate ,current_user: User = Depends(get_current_activ
 @router.get("/todos", response_model=list[TodoSchema])
 def read_todo(current_user: User = Depends(get_current_active_user), db: Session = Depends(get_db)):
     return db.query(Todo).filter(Todo.owner_id == current_user.id).all()
+
+@router.put("/todos/{todo_id}", response_model=TodoCreate)
+def update_todo(todo_id: int, todo_data: TodoUpdate, current_user: User = Depends(get_current_active_user), db: Session = Depends(get_db)):
+    todo = db.query(Todo).filter(Todo.id == todo_id, Todo.owner_id == current_user.id).first()
+    if not todo:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="todo not found")
+    
+    update_data = todo_data.dict(exclude_unset=True)
+    for field, value in update_data.items():
+        setattr(todo, field, value)
+
+    db.commit()
+    db.refresh(todo)
+    return todo
+
+@router.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int, current_user: User = Depends(get_current_active_user), db: Session = Depends(get_db)):
+    todo = db.query(Todo).filter(Todo.id == todo_id, current_user.id == Todo.owner_id).first()
+    if not todo:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="todo not found")
+    
+    db.delete(todo)
+    db.commit()
+    return {"message" : "todo deleted successfully"}
+
+    
--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Token(BaseModel):
     access_token : str
@@ -25,6 +26,11 @@ class TodoCreate(BaseModel):
     title : str
     description : str = None
     completed : bool = False
+
+class TodoUpdate(BaseModel):
+    title : Optional[str] = None
+    description : Optional[str] = None
+    completed : Optional[bool] = None
 
 class Todo(TodoCreate):
     id : int
