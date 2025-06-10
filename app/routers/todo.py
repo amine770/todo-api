b300381b9a7f5d8810fe9572d6d26bb57f19982f
target <aminesalem2223@gmail.com>
2025-06-10 16:28:18 +0100

    complete add the filter and sorte option

--- app/routers/todo.py
+++ app/routers/todo.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy import asc, desc
 from sqlalchemy.orm import Session
+from typing import Optional
 from ..database import get_db
 from ..models import Todo
 from ..schemas import TodoCreate, User, Todo as TodoSchema, TodoUpdate
@@ -15,9 +17,35 @@ def create_todo(todo: TodoCreate ,current_user: User = Depends(get_current_activ
     db.refresh(new_todo)
     return new_todo
 
+#read endpoints
 @router.get("/todos", response_model=list[TodoSchema])
 def read_todo(current_user: User = Depends(get_current_active_user), db: Session = Depends(get_db)):
     return db.query(Todo).filter(Todo.owner_id == current_user.id).all()
+
+@router.get("/todos/sorted", response_model=list[TodoSchema])
+def sorted_todo(sorted_by: Optional[str] = None, sorted_order: Optional[str] = "asc", current_user: User = Depends(get_current_active_user), db: Session = Depends(get_db)):
+    query = db.query(Todo).filter(Todo.owner_id == current_user.id)
+
+    if sorted_by:
+        if not hasattr(Todo, sorted_by):
+            raise HTTPException(
+                status_code = 400,
+                detail = f"Invalid sort filed : {sorted_by}"
+            )
+        
+        if sorted_order.lower() == "desc":
+            sort_func = desc
+        else:
+            sort_func = asc
+        
+        query = query.order_by(sort_func(getattr(Todo, sorted_by)))
+    
+    return query.all()
+
+@router.get("/todos/{filter_chars}", response_model=list[TodoSchema])
+def filter_todo(filter_chars: str, current_user: User = Depends(get_current_active_user), db: Session = Depends(get_db)):
+    return db.query(Todo).filter(Todo.title.startswith(filter_chars), current_user.id == Todo.owner_id)
+
 
 @router.put("/todos/{todo_id}", response_model=TodoCreate)
 def update_todo(todo_id: int, todo_data: TodoUpdate, current_user: User = Depends(get_current_active_user), db: Session = Depends(get_db)):
